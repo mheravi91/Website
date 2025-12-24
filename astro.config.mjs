d2ea2790bb08ba96a8f7c9cf18f35275da7b4e40
Mohammad Heravi <contact@mheravi.ir>
2025-12-25 00:34:32 +0200

    Changin domain from mheravi.ir to heravi.dev

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://mheravi.ir',
+  site: 'https://heravi.dev',
   integrations: [tailwind(), icon(), mdx(), react()],
   markdown: {
     shikiConfig: {
